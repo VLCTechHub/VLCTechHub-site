7615b6405786baed019dbb88a4b9ac848163bb98
hell03610 <eraseunavez@gmail.com>
2015-11-11 14:49:54 +0000

    Add link to calendar in event details

---
+++ app/components/link-to-calendar.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  urlToCalendar: Ember.computed('event', function() {
+    var zeroPad = function(number) {
+      return ("0" + number).slice(-2);
+    };
+    var isoDate = function(date) {
+      return date.getUTCFullYear() + zeroPad(date.getUTCMonth() + 1) + zeroPad(date.getUTCDate()) + 'T' +
+        zeroPad(date.getUTCHours()) + zeroPad(date.getUTCMinutes()) + zeroPad(date.getUTCSeconds()) + 'Z';
+    };
+
+    var title = encodeURIComponent(this.get('event.title'));
+    var details = encodeURIComponent('Más info: ' + this.get('event.link'));
+    var startDate = new Date(this.get('event.date'));
+    var endDate = new Date(this.get('event.date'));
+    endDate.setHours(endDate.getHours() + 1);
+    var dates = isoDate(startDate) + '/' + isoDate(endDate);
+
+    var url = 'http://www.google.com/calendar/event?action=TEMPLATE&text=' + title + '&dates=' + dates + '&details='+ details;
+    return url;
+  })
+});
