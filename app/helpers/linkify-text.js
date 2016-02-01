9ae1e5e7faac93350c9eba051b32d68418e0712f
hell03610 <eraseunavez@gmail.com>
2016-02-01 13:17:08 +0100

    Fix linkify for twitter hashtags

--- app/helpers/linkify-text.js
+++ app/helpers/linkify-text.js
@@ -7,11 +7,11 @@ export function linkfy([text]) {
   });
 
   text = text.replace(/(^|)@(\w+)/gi, function (s) {
-      return '<a href="http://twitter.com/' + s + '">' + s + '</a>';
+      return '<a href="https://twitter.com/' + s + '">' + s + '</a>';
   });
 
   text = text.replace(/(^|)#(\w+)/gi, function (s) {
-      return '<a href="http://search.twitter.com/search?q=' + s.replace(/#/,'%23') + '">' + s + '</a>';
+      return '<a href="https://twitter.com/hashtag/' + s.replace(/^#/,'') + '">' + s + '</a>';
   });
   return text;
 }
