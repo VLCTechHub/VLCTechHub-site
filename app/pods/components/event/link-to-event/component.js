893f7bc04f4f2fae1a66b88b40b4039caece77ea
hell03610 <eraseunavez@gmail.com>
2016-02-10 17:09:07 +0100

    [Upcoming list] Add expandable event description

--- app/pods/components/event/link-to-event/component.js
+++ app/pods/components/event/link-to-event/component.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'a',
   classNames: ['ml1'],
-  attributeBindings: ['href', 'target'],
+  attributeBindings: ['href', 'target', 'title'],
+  title: 'Ver fuente',
   target: '_blank',
   href: Ember.computed.alias('event.link')
 });
