18c4c1b4dedb3780f2e673d7430f9344d71b954b
hell03610 <eraseunavez@gmail.com>
2016-02-14 11:20:00 +0100

    Remove crazyegg. Add google analytics

--- config/tracking.js
+++ config/tracking.js
@@ -1,4 +1,7 @@
-setTimeout(function(){var a=document.createElement("script");
-var b=document.getElementsByTagName("script")[0];
-a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0045/8715.js?"+Math.floor(new Date().getTime()/3600000);
-a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-27446417-1', 'auto');
+ga('send', 'pageview');
