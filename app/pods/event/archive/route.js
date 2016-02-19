d5341a149d84388faf6e3c8fefa7e261849b84c9
hell03610 <eraseunavez@gmail.com>
2016-02-19 19:59:20 +0100

    Show loading when nagivating to archive

--- app/pods/event/archive/route.js
+++ app/pods/event/archive/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Route.extend({
-  redirect(model, transition) {
+  beforeModel() {
     var year = moment().format('YYYY');
     var month = moment().format('MM');
     this.transitionTo('event.archive.month', year, month);
