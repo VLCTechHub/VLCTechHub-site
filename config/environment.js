91d4cf5cbcf25c489517f9d0d8e88fde6f99e060
hell03610 <eraseunavez@gmail.com>
2016-08-07 09:44:21 +0000

    remove basscss, user purecss as base

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    ENV.APP.API_HOST = 'http://localhost:5000';
+//    ENV.APP.API_HOST = 'http://localhost:5000';
 
     ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
