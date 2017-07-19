1d30e4c8e8e58995141b021f30b9425960114de2
hell03610 <eraseunavez@gmail.com>
2017-07-19 11:28:13 +0000

    Track upcoming view via url query param

---
+++ app/pods/event/upcoming/controller.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  queryParams: ['view'],
+  isListVisible: Ember.computed('view', function() {
+    return this.get('view') !== 'calendar';
+  }),
+  isCalendarVisible: Ember.computed('view', function() {
+    return this.get('view') === 'calendar'
+  })
+});
--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    this.transitionTo('event.upcoming');
+    this.transitionTo('event.upcoming', {queryParams:{view: 'list'}});
   },
   model: function() {}
 });
